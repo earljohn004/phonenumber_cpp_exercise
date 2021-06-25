455f826ff2cf72342ee94230bb77c2f8b9ee2c3d
earljohn004 <earl.abaquita@gmail.com>
2021-06-25 18:35:54 +0800

    modified configuration file

--- src/prog_settings.cpp
+++ src/prog_settings.cpp
@@ -8,6 +8,10 @@ void prog_settings::open_config(){
 	pugi::xml_document doc;
 	pugi::xml_parse_result result = doc.load_file("config.xml");
 
+	if(!result){
+		MESSAGE_LOG("[ERROR] file not found. loading default configuration");
+	}
+
 	auto included = doc.child("config").child("input_setting").child("included").first_child();
 	for( included.first_child(); included ; included = included.next_sibling()){
 		std::cout << included.name() << std::endl;
